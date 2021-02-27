299503eb084870a31386bac9969658228aefce13
Rinat Khantimirov <khantimirov.rinat@gmail.com>
2021-02-27 20:54:50 +0300

    Базовый функционал

---
+++ main.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import os
+import unittest
+from envparse import env
+from selenium import webdriver
+from selenium_stealth import stealth
+from page import MailRu
+
+currentDir = os.path.dirname(os.path.abspath(__file__))
+env.read_envfile(currentDir + '/.env')
+
+MAILRU_LOGIN_LINK = os.environ['MAILRU_LOGIN_LINK']
+MAILRU_USERNAME = os.environ['MAILRU_USERNAME']
+MAILRU_PASSWORD = os.environ['MAILRU_PASSWORD']
+NAME_FROM = os.environ['NAME_FROM']
+STUDENT_NAME = os.environ['STUDENT_NAME']
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+
+WEBDRIVER = webdriver.Chrome(
+    options=options,
+    executable_path="/usr/bin/chromedriver")
+
+stealth(WEBDRIVER,
+        languages=["ru-RU", "ru"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
+
+
+class MailRuTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = WEBDRIVER
+        self.page = MailRu(self.driver)
+
+    def test_Mail(self):
+        self.load_site(MAILRU_LOGIN_LINK)
+        self.login(MAILRU_USERNAME, MAILRU_PASSWORD)
+        emailsCount = self.findAndCountEmails(NAME_FROM)
+
+        body = "От вас пришло писем - {}".format(emailsCount)
+        subject = "Тестовое задание {}".format(STUDENT_NAME)
+
+        self.sendEmail(NAME_FROM, body, subject)
+
+    def load_site(self, url: str):
+        titleLogin = self.page.goToUrl(url)
+        self.assertEqual(titleLogin, "Авторизация")
+
+    def login(self, userName, passwrd):
+        titleMail = self.page.login(userName, passwrd)
+        self.assertRegex(titleMail, "Входящие - Почта Mail.ru")
+
+    def findAndCountEmails(self, fromWho):
+        countEmails = self.page.findAndCountEmails(fromWho)
+        self.assertGreater(countEmails, 0)
+        return countEmails
+
+    def sendEmail(self, toWho, body, subject):
+        result = self.page.sendEmail(nameoOrAdress=toWho,
+                                     textBody=body,
+                                     subject=subject)
+        self.assertTrue(result)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ page.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+import random
+from time import sleep
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+
+class BasePage(object):
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def goToUrl(self, url: str):
+        assert isinstance(url, str)
+        self.driver.get(url)
+        sleep(random.uniform(1.0, 3.0))
+        title = self.driver.title
+        assert isinstance(title, str)
+        return title
+
+    def findAndAction(self,
+                      xpathStr: str = None,
+                      returnKey: bool = False,
+                      click: bool = False,
+                      stringToType: str = None):
+
+        wait = WebDriverWait(self.driver, 10)
+        if xpathStr is not None:
+            element = wait.until(
+                EC.visibility_of_element_located(
+                    (By.XPATH, xpathStr)))
+        else:
+            element = self.driver.switch_to.active_element
+        if click:
+            element.click()
+
+        if stringToType is not None:
+            for key in stringToType:
+                element.send_keys(key)
+                sleep(random.uniform(0.02, 0.1))
+        if returnKey:
+            sleep(random.uniform(0.5, 0.1))
+            element.send_keys(Keys.RETURN)
+        sleep(random.uniform(2.0, 3.0))
+        return element
+
+
+class MailRu(BasePage):
+
+    def login(self,
+              MailRuUsername: str,
+              MailRuPassword: str):
+        assert isinstance(MailRuUsername, str)
+        assert isinstance(MailRuPassword, str)
+        # Вводим имя пользователя
+        self.findAndAction(
+            xpathStr='//input[@name="username"]',
+            returnKey=True,
+            stringToType=MailRuUsername)
+        # Вводим пароль
+        self.findAndAction(
+            xpathStr='//input[@name="password"]',
+            returnKey=True,
+            stringToType=MailRuPassword)
+
+        sleep(random.uniform(3.0, 5.0))
+        title = self.driver.title
+        assert isinstance(title, str)
+        return title
+
+    def findAndCountEmails(self, nameFrom: str):
+        assert isinstance(nameFrom, str)
+
+        self.findAndAction(
+            xpathStr='//span[@class ="search-panel-button__text"]',
+            click=True)
+
+        self.findAndAction(
+            xpathStr='//input',
+            returnKey=True,
+            stringToType=nameFrom)
+
+        mails = self.driver.find_elements(By.XPATH, '//span[@class="ll-crpt"]')
+        tags = self.driver.find_elements(By.XPATH, '//div[@class="ll-f"]')
+
+        correctEmailsCount = 0
+        i = 0
+        for mail in mails:
+            nameInTitle = nameFrom in mail.get_attribute("title")
+            inbox = tags[i].get_attribute("title") == "Входящие"
+            if nameInTitle and inbox:
+                correctEmailsCount += 1
+            i += 1
+        sleep(random.uniform(3.0, 5.0))
+        assert isinstance(correctEmailsCount, int)
+        return correctEmailsCount
+
+    def sendEmail(self, subject: str, textBody: str,
+                  nameoOrAdress: str,
+                  ):
+        assert isinstance(subject, str)
+        assert isinstance(textBody, str)
+        assert isinstance(nameoOrAdress, str)
+
+        self.findAndAction(
+            xpathStr='//span[text() ="Написать письмо"]',
+            click=True)
+
+        self.findAndAction(
+            returnKey=True,
+            stringToType=nameoOrAdress)
+
+        self.findAndAction(
+            xpathStr='//input[@name = "Subject"]',
+            returnKey=True,
+            stringToType=subject)
+
+        self.findAndAction(
+            xpathStr='//div[@contenteditable = "true"]',
+            returnKey=True,
+            stringToType=textBody)
+
+        self.findAndAction(
+            xpathStr='//span[text() = "Отправить"]',
+            click=True)
+
+        sleep(random.uniform(3.0, 5.0))
+
+        result = self.findAndAction(
+            xpathStr='//a[text() = "Письмо отправлено"]')
+
+        assert result is not None
+
+        return True
+
+
+class Gmail(BasePage):
+    pass
