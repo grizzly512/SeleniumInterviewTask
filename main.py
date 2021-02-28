af0b7d1771275f7fdd99cd5188db95649cda1930
Rinat Khantimirov <khantimirov.rinat@gmail.com>
2021-02-28 17:58:33 +0300

    Добавил комментарии

--- main.py
+++ main.py
@@ -5,26 +5,31 @@ import unittest
 import allure
 from envparse import env
 from selenium import webdriver
-from selenium_stealth import stealth
+# from selenium_stealth import stealth
 from page import MailRu
-
+# Читаем данные из окружения
 currentDir = os.path.dirname(os.path.abspath(__file__))
 env.read_envfile(currentDir + '/.env')
-
+# Константы из окружения, можно определить здесь:
+# Ссылка на страницу входа mail.ru
 MAILRU_LOGIN_LINK = os.environ['MAILRU_LOGIN_LINK']
+# Логин mail.ru
 MAILRU_USERNAME = os.environ['MAILRU_USERNAME']
+# Пароль mail.ru
 MAILRU_PASSWORD = os.environ['MAILRU_PASSWORD']
+# Адрес или имя адресата
 NAME_FROM = os.environ['NAME_FROM']
-STUDENT_NAME = os.environ['STUDENT_NAME']
+# Фамилия студента
+STUDENT_SNAME = os.environ['STUDENT_SNAME']
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
+# Настройки локального драйвера. просто оставлю тут, на всякий случай
 
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("excludeSwitches", ["enable-automation"])
+# options.add_experimental_option('useAutomationExtension', False)
 # WEBDRIVER_LOCAL = webdriver.Chrome(
 #     options=options,
 #     executable_path="/usr/bin/chromedriver")
-
 # stealth(WEBDRIVER_LOCAL,
 #         languages=["ru-RU", "ru"],
 #         vendor="Google Inc.",
@@ -34,18 +39,38 @@ options.add_experimental_option('useAutomationExtension', False)
 #         fix_hairline=True,
 #         )
 
+# Удаленный вэбдрайвер
 desiredCapabilities = {"browserName": "chrome"}
-
 WEBDRIVER_REMOTE = webdriver.Remote(desired_capabilities=desiredCapabilities)
 
+
 class MailRuTest(unittest.TestCase):
+    """
+    Класс тестов с mail.ru
+    inheritance: unittest.TestCase
+    methods:
+        public:
+            setUp
+            test_Mail
+            load_site
+            login
+            findAndCountEmails
+            sendEmail
+            tearDown
+    """
 
     def setUp(self):
+        """
+        Настройка тестов
+        """
         self.driver = WEBDRIVER_REMOTE
         self.driver.maximize_window()
         self.page = MailRu(self.driver)
 
     def test_Mail(self):
+        """
+        Входной тест с шагами allure
+        """
         with allure.step("Загрузка сайта"):
             self.load_site(MAILRU_LOGIN_LINK)
         with allure.step("Логин"):
@@ -54,30 +79,67 @@ class MailRuTest(unittest.TestCase):
             emailsCount = self.findAndCountEmails(NAME_FROM)
 
         body = "От вас пришло писем - {}".format(emailsCount)
-        subject = "Тестовое задание {}".format(STUDENT_NAME)
+        subject = "Тестовое задание {}".format(STUDENT_SNAME)
         with allure.step("Отправка письма"):
             self.sendEmail(NAME_FROM, body, subject)
 
     def load_site(self, url: str):
+        """
+        Загрузка сайта
+        arguments:
+            url: str - адрес страницы
+        returns:
+            None
+        """
+        # Предусловия в self.page.goToUrl()
         titleLogin = self.page.goToUrl(url)
+        # Постусловия
         self.assertEqual(titleLogin, "Авторизация")
 
     def login(self, userName, passwrd):
+        """
+        Логин на mail.ru
+        arguments:
+            userName: str, - логин mail.ru
+            passwrd: str, - пароль mail.ru
+        """
+        # Предусловия в self.page.login()
         titleMail = self.page.login(userName, passwrd)
+        # Постусловия
         self.assertRegex(titleMail, "Входящие - Почта Mail.ru")
 
     def findAndCountEmails(self, fromWho):
+        """
+        Найти и посчитать входящие электронные письма от адресата
+        arguments:
+            fromWho: str, - имя или адрес
+        returns:
+            countEmails: str - количесто эл. писем
+        """
+        # Предусловия в self.page.findAndCountEmails()
         countEmails = self.page.findAndCountEmails(fromWho)
+        # Постусловия
         self.assertGreater(countEmails, 0)
         return countEmails
 
     def sendEmail(self, toWho, body, subject):
+        """
+        Отправить письмо
+        arguments:
+            subject: str, - Тема письма
+            body: str, - Тескт письма
+            toWho: str, - Имя или адрес
+        """
         result = self.page.sendEmail(nameoOrAdress=toWho,
                                      textBody=body,
                                      subject=subject)
+        # Постусловия
         self.assertTrue(result)
 
     def tearDown(self):
+        """
+        Действия по завершению
+        """
         self.driver.close()
 
 
--- page.py
+++ page.py
@@ -6,86 +6,165 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class BasePage(object):
+    """
+    Базовый класс для страниц
+    init: driver - Selenium web driver
+    methods:
+        public:
+            goToUrl
+        private:
+            findAndAaction
+    """
 
     def __init__(self, driver):
+        """Инициализация класса"""
         self.driver = driver
 
-    def goToUrl(self, url: str):
+    def goToUrl(self, url: str) -> str:
+        """
+        Перейти к URL
+        arguments:
+            url: str - адрес страницы
+        returns:
+            title: str - тайтл полученной страницы
+        """
+        # Предусловия
         assert isinstance(url, str)
         self.driver.get(url)
         sleep(random.uniform(1.0, 3.0))
+        # Постусловия
         title = self.driver.title
         assert isinstance(title, str)
         return title
 
-    def findAndAction(self,
-                      xpathStr: str = None,
-                      returnKey: bool = False,
-                      click: bool = False,
-                      stringToType: str = None):
-
+    def _findAndAction(self,
+                       xpathStr: str = None,
+                       returnKey: bool = False,
+                       click: bool = False,
+                       stringToType: str = None) -> WebElement:
+        """
+        Найти элемент по локатору и выполнить действия над ним
+        arguments:
+            xpathStr: str = None, - xpath локатор
+            returnKey: bool = False, - нажимать ли пробел?
+            click: bool = False, - кликать на элемент?
+            stringToType: str = None - текст для печати в форму
+        returns:
+            element: selemium.webdriver.WebElement
+        """
+        # Предусловия
+        xpathStrAssert = (
+            isinstance(xpathStr, str) or xpathStr is None)
+        stringToTypeAssert = (
+            isinstance(stringToType, str) or stringToType is None)
+        assert xpathStrAssert
+        assert stringToTypeAssert
+        assert isinstance(returnKey, bool)
+        assert isinstance(click, bool)
+        # Находим элемент по локатору, если xpathStr не None
         wait = WebDriverWait(self.driver, 10)
         if xpathStr is not None:
             element = wait.until(
                 EC.visibility_of_element_located(
                     (By.XPATH, xpathStr)))
+        # Либо назначаем текущий элемент активным
         else:
             element = self.driver.switch_to.active_element
+        # Кликаем, если нужно
         if click:
             element.click()
-
+        # Вписываем текст, если нужно
         if stringToType is not None:
             for key in stringToType:
                 element.send_keys(key)
                 sleep(random.uniform(0.02, 0.1))
+        # Нажимаем пробел, если нужно
         if returnKey:
             sleep(random.uniform(0.5, 0.7))
             element.send_keys(Keys.RETURN)
         sleep(random.uniform(2.0, 3.0))
+        # Постусловия
+        assert isinstance(element, WebElement)
         return element
 
 
+# TODO: Мультиязычность.
+# TODO: Стабильная работа при уменьшении размера окна.
 class MailRu(BasePage):
+    """
+    Класс работы с mail.ru
+    inheritance: BasePage
+    init: driver - Selenium web driver
+    methods:
+        public:
+            goToUrl
+            login
+            findAndCountEmails
+            sendEmail
+        private:
+            findAndAaction
+    """
 
     def login(self,
               MailRuUsername: str,
-              MailRuPassword: str):
+              MailRuPassword: str) -> str:
+        """
+        Логин на mail.ru
+        arguments:
+            MailRuUsername: str, - логин mail.ru
+            MailRuPassword: str, - пароль mail.ru
+        returns:
+            title: str - тайтл полученной страницы
+        """
+        # Предусловия
         assert isinstance(MailRuUsername, str)
         assert isinstance(MailRuPassword, str)
         # Вводим имя пользователя
-        self.findAndAction(
+        self._findAndAction(
             xpathStr='//input[@name="username"]',
             returnKey=True,
             stringToType=MailRuUsername)
         # Вводим пароль
-        self.findAndAction(
+        self._findAndAction(
             xpathStr='//input[@name="password"]',
             returnKey=True,
             stringToType=MailRuPassword)
 
         sleep(random.uniform(5.0, 7.0))
+        # Постусловия
         title = self.driver.title
         assert isinstance(title, str)
         return title
 
-    def findAndCountEmails(self, nameFrom: str):
+    def findAndCountEmails(self, nameFrom: str) -> int:
+        """
+        Найти и посчитать входящие электронные письма от адресата
+        arguments:
+            nameFrom: str, - имя или адрес
+        returns:
+            correctEmailsCount: int - количество входящих сообщений
+        """
+        # Предусловия
         assert isinstance(nameFrom, str)
-
-        self.findAndAction(
+        # Находим и кликаем на кнопку поиска
+        self._findAndAction(
             xpathStr='//span[@class ="search-panel-button__text"]',
             click=True)
-
-        self.findAndAction(
+        # Находим строку поиска и вводим имя отправителя
+        # TODO: Победить infinite scroll
+        self._findAndAction(
             xpathStr='//input',
             returnKey=True,
             stringToType=nameFrom)
-
+        # Считаем письма и тэги
         mails = self.driver.find_elements(By.XPATH, '//span[@class="ll-crpt"]')
         tags = self.driver.find_elements(By.XPATH, '//div[@class="ll-f"]')
-
+        # Считаем только необходимые письма
+        # TODO: Считать не только входящие, но и исходящие
         correctEmailsCount = 0
         i = 0
         for mail in mails:
@@ -95,47 +174,65 @@ class MailRu(BasePage):
                 correctEmailsCount += 1
             i += 1
         sleep(random.uniform(3.0, 5.0))
+        # Постусловия
         assert isinstance(correctEmailsCount, int)
         return correctEmailsCount
 
     def sendEmail(self, subject: str, textBody: str,
                   nameoOrAdress: str,
-                  ):
+                  ) -> bool:
+        """
+        Отправить письмо
+        arguments:
+            subject: str, - Тема письма
+            textBody: str, - Тескт письма
+            nameoOrAdress: str, - Имя или адрес
+        returns:
+            True
+        """
+        # Предусловия
         assert isinstance(subject, str)
         assert isinstance(textBody, str)
         assert isinstance(nameoOrAdress, str)
-
-        self.findAndAction(
+        # Находим кнопку "Написать письмо" и кликаем на нее
+        self._findAndAction(
             xpathStr='//span[text() ="Написать письмо"]',
             click=True)
-
-        self.findAndAction(
+        # Фокус переходит на строку адреса, поэтому не нужно ее искать,
+        # просто вводим имя получателя и нажимаем Enter
+        self._findAndAction(
             returnKey=True,
             stringToType=nameoOrAdress)
-
-        self.findAndAction(
+        # Ищем поле с темой письма и вбиваем тему письма
+        self._findAndAction(
             xpathStr='//input[@name = "Subject"]',
             returnKey=True,
             stringToType=subject)
-
-        self.findAndAction(
+        # Ищем поле для ввода текста и вбиваем текст письма
+        self._findAndAction(
             xpathStr='//div[@contenteditable = "true"]',
             returnKey=True,
             stringToType=textBody)
-
-        self.findAndAction(
+        # Нажимаем кнопку отправить
+        self._findAndAction(
             xpathStr='//span[text() = "Отправить"]',
             click=True)
 
         sleep(random.uniform(3.0, 5.0))
-
-        result = self.findAndAction(
+        # Постусловия
+        result = self._findAndAction(
             xpathStr='//a[text() = "Письмо отправлено"]')
 
         assert result is not None
-
+        # Если нашли result, то возвращаем True, если нет, то
+        # assert все равно не пропустит
         return True
 
 
 class Gmail(BasePage):
+    """
+    Потратил много времени на поиск обходного пути на gmail
+    пробовал скрываться, входить через сторонние сайты,
+    на 28.02.21 действующих способов не нашел.
+    """
     pass
